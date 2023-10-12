9986a0d2d1cb1802760bfab1500b8b164186e744
hvergote <henry.vergote@student.hogent.be>
2023-10-12 18:50:38 +0200

    Initial Commit

---
+++ VisionOogcentrum/VisionOogcentrumApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionOogcentrumApp.swift
+//  VisionOogcentrum
+//
+//  Created by Henry Vergote on 12/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct VisionOogcentrumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
